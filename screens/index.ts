07ba64cab8e37fe8e1acc144d9b5868d9b6edd7f
Paulinha <paulafabianasoares@gmail.com>
2024-07-14 17:52:16 -0300

    [add] index screens

---
+++ screens/index.ts
@@ -0,0 +1,17 @@
+import { LoginScreen } from "../navigators/(public)/LoginScreen";
+import { HomeScreen } from "../navigators/(private)/HomeScreen";
+import WhoAreWe from "../navigators/(private)/WhoAreWe";
+import Profile from "../navigators/(private)/Profile";
+import Settings from "../navigators/(private)/Settings";
+import OurOptionsOne from "../navigators/(private)/OurOptionsOne";
+import OurOptionsTwo from "../navigators/(private)/OurOptionsTwo";
+
+export const Screens = {
+  Login: LoginScreen,
+  Home: HomeScreen,
+  WhoAreWe: WhoAreWe,
+  OurOptionsOne: OurOptionsOne,
+  OurOptionsOneTwo: OurOptionsTwo,
+  Profile: Profile,
+  Settings: Settings,
+};
